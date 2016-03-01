523aa6749d9527ec840790d2cf30b67cbe3e15c1
GeorageLiang <callmelianghaoxu@gmail.com>
2016-03-01 16:35:55 +0800

    add 4page raphael table

--- js/d3.js
+++ js/d3.js
@@ -1,6 +1,7 @@
 /**
  * Created by Geo on 16/2/16.
  */
+var $ = require("jquery");
 var paperSet = [];
 
 var paintingline1 = function(obj){
@@ -93,8 +94,27 @@ var clean = function(){
     }
 }
 var paintMap = function(){
-    var r = Raphael("holder", 850, 600),
+    var r = Raphael("holder", 600, 600),
         R = 250, param = {stroke: "red", "stroke-width": 30};
+
+
+    var r1 = Raphael("techs", 600, 700);
+    var tech1 = [{
+        name:"1",
+        num:10
+    },{
+        name:"1",
+        num:20
+    },{
+        name:"1",
+        num:30
+    },{
+        name:"1",
+        num:50
+    },{
+        name:"1",
+        num:80
+    }];
 
     r.customAttributes.arc = function (value, total, R,center,color) {
         var alpha = 360 / total * value,
@@ -119,28 +139,78 @@ var paintMap = function(){
         sec3.animate({"stroke-width":"30","stroke-opacity":"1"},500,"bounce");
         sec4.animate({"stroke-width":"30","stroke-opacity":"1"},500,"bounce");
     };
-    var sec1 = r.path().attr(param).attr({arc: [10, 100, R-105,false,"hsb(0.21,1,0.81)"]}).click(function(){
+
+    var eventTrigger = function(obj,view,datas,color){
         cleanAnmit();
-       this.animate({"stroke-width":"70","stroke-opacity":"0.8"},500,"bounce");
+        obj.animate({"stroke-width":"80","stroke-opacity":"0.8"},500,"bounce");
+        //drawTechs(r1,tech1,"#97d000");
+         drawTechs(view,datas,color);
+    }
+
+    var sec1 = r.path().attr(param).attr({arc: [10, 100, R-105,false,"hsb(0.21,1,0.81)"]}).click(function(){
+        eventTrigger(this,r1,tech1,"#97d000");
+        centerTxt.attr({"text":"Tools"});
     });
     var sec2 = r.path().attr(param).attr({arc: [30, 100, R-70,false,"hsb(0.82,0.28,1)"]}).click(function(){
-        cleanAnmit();
-        this.animate({"stroke-width":"70","stroke-opacity":"0.8"},500,"bounce");
+        eventTrigger(this,r1,tech1,"#fab5ff");
+        centerTxt.attr({"text":"Common"});
     });
     var sec3 = r.path().attr(param).attr({arc: [50, 100, R-35,false,"hsb(0.63,0.65,0.84)"]}).click(function(){
-        cleanAnmit();
-        this.animate({"stroke-width":"70","stroke-opacity":"0.8"},500,"bounce");
+        eventTrigger(this,r1,tech1,"#4866d9");
+        centerTxt.attr({"text":"Advanced"});
     });
     var sec4 = r.path().attr(param).attr({arc: [60, 100, R,false,"hsb(0.54,0.86,0.8)"]}).click(function(){
-        cleanAnmit();
-        this.animate({"stroke-width":"70","stroke-opacity":"0.8"},500,"bounce");
+        eventTrigger(this,r1,tech1,"#4fa4d9");
+        centerTxt.attr({"text":"FE-Basic"});
     });
     var sec5 = r.path().attr(param).attr({arc: [100, 100, R-140,true]}).click(function(){
         cleanAnmit();
+        r1.clear();
+        centerTxt.attr({"text":"Skills"});
     });
-    r.text("300","300","Skills").attr({"font-size":"50px","fill":"white","stroke":"white" ,"text-anchor":"center"});
+    var centerTxt = r.text("300","300","Skills").attr({"font-size":"50px","fill":"white","stroke":"white" ,"text-anchor":"center"});
+
+    centerTxt.click(function(){
+        cleanAnmit();
+        r1.clear();
+    });
+
+    $(".btn").eq(3).on("click",function(){
+        eventTrigger(sec1,r1,tech1,"#97d000");
+        centerTxt.attr({"text":"Tools"});
+    });
+    $(".btn").eq(2).on("click",function(){
+        eventTrigger(sec2,r1,tech1,"#fab5ff");
+        centerTxt.attr({"text":"Common"});
+    });
+    $(".btn").eq(1).on("click",function(){
+        eventTrigger(sec3,r1,tech1,"#4866d9");
+        centerTxt.attr({"text":"Advanced"});
+    });
+    $(".btn").eq(0).on("click",function(){
+        eventTrigger(sec4,r1,tech1,"#4fa4d9");
+        centerTxt.attr({"text":"FE-Basic"});
+    });
+
+    /*--techs---*/
+
+    var drawTechs = function(r1,data,color){
+        r1.clear();
+        var l = data.length;
+
+        for(var i in data){
+            var lastNum = (data[i].num)/100*800+80;
+            var add = 60+60*i
+            console.log(lastNum);
+            r1.path("M80,"+add).attr({"stroke-width":"40","stroke-opacity":"0.8","stroke":color}).animate({"path":"M80,"+add+"L"+lastNum+","+add},Math.random()*2500,"bounce");
+            r1.text("120",add,data[i].name).attr({"font-size":"25px","fill":"white","stroke":"white" ,"text-anchor":"center"});
+        }
+
+    }
+
 
 }
+
 
 
 module.exports = {
