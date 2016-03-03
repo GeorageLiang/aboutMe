236d897500cdf326cacd8cd2796eaadfce95bdea
GeorageLiang <callmelianghaoxu@gmail.com>
2016-03-03 17:17:39 +0800

    update

--- js/min/all.js
+++ js/min/all.js
@@ -48,10 +48,9 @@
 	 * Created by Geo on 16/2/16.
 	 */
 
-
-
 	var $ = __webpack_require__(1);
 	__webpack_require__(2);
+
 	var painting = __webpack_require__(3);
 
 	$(document).ready(function() {
@@ -67,12 +66,14 @@
 	                        $(".bg").addClass("animated fadeInUp");
 	                        $(".title1").removeClass("hide").addClass("animated fadeInUp");
 	                        $(".sub-title1").removeClass("hide").addClass("animated fadeInUp");
-	                        painting.paintingline1(d3.select("#section1"));
-	                        painting.paintingTr(d3.select("#section1"));
+	                        painting.paintingline1("section1");
+	                        painting.paintingTr("section1");
+
+
 	                    }
 	                    if (index == 2) {
-	                        painting.paintingline2(d3.select("#section2"));
-	                        $("#backimg2").fadeIn(800,function(){
+	                        painting.paintingline2("section2");
+	                        $("#backimg2").animate({"opacity":1},800,function(){
 	                            $(".intro-title").animate({"opacity":1},1000,"linear",function(){
 	                                $(".intro-body").addClass("animated fadeInUp");
 	                            })
@@ -82,13 +83,33 @@
 	                    }
 
 	                    if (index == 3) {
-	                        painting.paintingline1(d3.select("#section3"));
-	                        painting.paintingTr(d3.select("#section3"));
+	                        //painting.paintingline1("section3");
+	                        //$(".flex-item").addClass("animated fadeInUp");
+
+	                        //painting.paintingTr(d3.select("#section3"));
+	                        //painting.paintingline3("section3");
+	                        $(".flex-item").eq(0).animate({"opacity":1,"top":"-2em"},500,"linear",function(){
+	                            $(".flex-item").eq(1).animate({"opacity":1,"top":"-2em"},500,"linear",function(){
+	                                $(".flex-item").eq(2).animate({"opacity":1,"top":"-2em"},500,"linear",function(){
+	                                    $(".flex-item").eq(3).animate({"opacity":1,"top":"-2em"},500,"linear").delay(300);
+	                                }).delay(300);
+	                            }).delay(300);
+	                        }).delay(300);
 	                    }
 
 	                    if (index == 4) {
-	                        painting.paintingline1(d3.select("#section4"));
-	                        painting.paintingTr(d3.select("#section4"));
+	                        //painting.paintingline1("section4");
+	                        //painting.paintingTr("section4");
+	                        painting.paintMap(function(){
+	                            $("#section4").animate({"opacity":1},800);
+	                        });
+	                    }
+	                    if (index == 5) {
+
+	                    }
+	                    if (index == 6) {
+	                       $(".contact").addClass("animated rubberBand");
+	                       $(".phone-active").addClass("animated pulse");
 	                    }
 
 	                },
@@ -105,15 +126,21 @@
 	                            break;
 	                        case 2:
 	                            $(".intro-body").removeClass("animated fadeInUp");
-	                            $("#backimg2").fadeOut(200);
-	                            $(".intro-title").css("opacity",0);
+	                            $("#backimg2").stop().css("opacity",0);
+	                            $(".intro-title").stop().css("opacity",0);
 	                            painting.clean();
 	                            break;
 	                        case 3:
+	                            $(".flex-item").stop().css({"opacity":0,"top":0});
 	                            painting.clean();
 	                            break;
 	                        case 4:
+	                            $("#section4").stop().animate({"opacity":0},200);
 	                            painting.clean();
+	                            break;
+	                        case 6:
+	                            $(".contact").removeClass("animated rubberBand");
+	                            $(".phone-active").removeClass("animated pulse");
 	                            break;
 	                    }
 	                }
@@ -12755,19 +12782,20 @@
 
 /***/ },
 /* 3 */
-/***/ function(module, exports) {
+/***/ function(module, exports, __webpack_require__) {
 
 	/**
 	 * Created by Geo on 16/2/16.
 	 */
+	var $ = __webpack_require__(1);
+	var paperSet = [];
 
 	var paintingline1 = function(obj){
 	    //d3.select(".section").selectAll("h3").text("www11com");
 	    var width = 300;  //画布的宽度
 	    var height = 300;   //画布的高度
-	    var datasety = [300 , 250]; //y11,y21
-	    paintinglineUp(obj,width,height,datasety,{"position":"absolute","top":"8%","right":0},"steelblue");
-	    paintinglineDown(obj,width,height,datasety,{"position":"absolute","bottom":"20%","left":0},"steelblue");
+	    paintinglineUp(obj,width,height,{top:"8%",right:0},"steelblue");
+	    paintinglineDown(obj,width,height,{bottom:"20%",left:0},"steelblue");
 	}
 
 	var paintingline2 = function(obj){
@@ -12775,117 +12803,266 @@
 	    var width = 300;  //画布的宽度
 	    var height = 300;   //画布的高度
 	    var datasety = [300 , 250]; //y11,y21
-	    paintinglineUp(obj,width,height,datasety,{"position":"absolute","top":"8%","right":0},"#f8ccfc");
-	    paintinglineDown(obj,width,height,datasety,{"position":"absolute","bottom":"50%","left":0},"#f8ccfc");
+	    paintinglineUp(obj,width,height,{top:"8%",right:0},"#f8ccfc");
+	    paintinglineDown(obj,width,height,{bottom:"50%",left:0},"#f8ccfc");
 	}
 
-	var paintinglineDown = function(obj,width,height,datasety,stylle,color){
+	var paintinglineUp = function(obj,width,height,stylle,color){
 
-	    var svg1 = obj     //选择文档中的body元素
-	        .append("svg")          //添加一个svg元素
-	        .attr("width", width)       //设定宽度
-	        .attr("height", height)
-	        .style(stylle);
-	    svg1.append("line");
-	    svg1.append("line");
+	    var svg1 = document.createElement("div");
+	    svg1.style.position = "absolute";
+	    svg1.style.top = stylle.top;
+	    svg1.style.right = stylle.right;
 
-	    svg1.selectAll("line").data(datasety).attr("x1",0)
-	        .attr("y1",function(d,i){
-	            return d;
-	        }).attr("x2",0).attr("y2",function(d,i){
-	            return d;
-	        })
+	    document.getElementById(obj).appendChild(svg1);
 
-	        .attr("stroke",color).attr("stroke-width","5").transition().duration(1500).delay(500).attr("x2",function(d,i){
-	            if(i == 0){
-	                return 300;
-	            }else {
-	                return 150;
-	            }
-	        })
-	        .attr("y2",function(d,i){
-	            if(i == 0){
-	                return 0;
-	            }else {
-	                return 100;
-	            }
-	        });
+	    var svgA = Raphael(svg1, width, height);
+	    var cA1 = svgA.path("M300 0").attr({"stroke":color,"stroke-width":4});
+	    var cA2 = svgA.path("M300 50").attr({"stroke":color,"stroke-width":4});
+	    var animationA1 = Raphael.animation({"path":"M300 0 L0 300"},1500,"cubic-bezier(0.77, 0, 0.175, 1)").delay(500);
+	    var animationA2 = Raphael.animation({"path":"M300 50 L150 200"},1500,"cubic-bezier(0.77, 0, 0.175, 1)").delay(500);
+	    cA1.animate(animationA1);
+	    cA2.animate(animationA2);
+	    paperSet.push(svgA);
+
 	}
 
-	var paintinglineUp = function(obj,width,height,datasety,stylle,color){
-	    var svg2 = obj     //选择文档中的body元素
-	        .append("svg")          //添加一个svg元素
-	        .attr("width", width)       //设定宽度
-	        .attr("height", height)//设定高度
-	        .style(stylle);
+	var paintinglineDown = function(obj,width,height,stylle,color){
 
-	    svg2.append("line");
-	    svg2.append("line");
+	    var svg2 =  document.createElement("div");
+	    svg2.style.position = "absolute";
+	    svg2.style.bottom = stylle.bottom;
+	    svg2.style.left = stylle.left;
 
-	    svg2.selectAll("line").data(datasety).attr("x1",300)
-	        .attr("y1",function(d,i){
-	            if(i == 0){
-	                return 0;
-	            }else {
-	                return 50;
-	            }
-	        })
-	        .attr("x2",300)
-	        .attr("y2",function(d,i){
-	            if(i == 0){
-	                return 0;
-	            }else {
-	                return 50;
-	            }
-	        })
-	        .attr("stroke",color).attr("stroke-width","5").transition().duration(1500).delay(500).attr("x2",function(d,i){
-	            if(i==0){
-	                return 0
-	            }else{
-	                return 150
-	            }
-	        })
-	        .attr("y2",function(d,i){
-	            if(i==0){
-	                return 300
-	            }else{
-	                return 200
-	            }
-	        });
+	    document.getElementById(obj).appendChild(svg2);
 
+	    var svgB = Raphael(svg2, width, height);
+	    var cB1 = svgB.path("M0 300").attr({"stroke":color,"stroke-width":4});
+	    var cB2 = svgB.path("M0 250").attr({"stroke":color,"stroke-width":4});
+	    var animationB1 = Raphael.animation({"path":"M0 300 L300 0"},1500,"cubic-bezier(0.77, 0, 0.175, 1)").delay(500);
+	    var animationB2 = Raphael.animation({"path":"M0 250 L150 100"},1500,"cubic-bezier(0.77, 0, 0.175, 1)").delay(500);
+	    cB1.animate(animationB1);
+	    cB2.animate(animationB2);
+	    paperSet.push(svgB);
 	}
 
 
 	var paintingTr = function(obj){
 	    var width = 300;  //画布的宽度
-	    var height = 300;   //画布的高度
-	    var svg3 = obj     //选择文档中的body元素
-	        .append("svg")          //添加一个svg元素
-	        .attr("width", width)       //设定宽度
-	        .attr("height", 900)
-	        .style({"position":"absolute","top":0,"left":0});
+	    var height = 900;   //画布的高度
+	    var svg3 =  document.createElement("div");
+	    svg3.style.position = "absolute";
+	    svg3.style.top = 0;
+	    svg3.style.left = 0;
 
-	    svg3.append("path").attr("d"," M 60 205 L 130 165 L 140 275 L 60 205").attr("stroke","#3981cb").attr("stroke-width","2").attr("fill","#3981cb")
-	        .style("opacity",0).transition().duration(1000).delay(500).style("opacity",1);
-	    svg3.append("path").attr("d"," M 90 365 L 100 315 L 130 335 L 90 365").attr("stroke","#3981cb").attr("stroke-width","2").attr("fill","#3981cb")
-	        .style("opacity",0).transition().duration(1000).delay(1000).style("opacity",0.7);
-	    svg3.append("path").attr("d"," M 70 475 L 130 555 L 200 440").attr("stroke","#3981cb").attr("stroke-width","2").attr("fill","#3981cb")
-	        .style({"opacity":0,}).transition().duration(1000).delay(1500).style("opacity",1);
-	    svg3.append("path").attr("d"," M 40 625 L 60 655 L 70 625").attr("stroke","#3981cb").attr("stroke-width","2").attr("fill","#3981cb")
-	        .style("opacity",0).transition().duration(1000).delay(2000).style("opacity",0.8);
+	    document.getElementById(obj).appendChild(svg3);
+	    var svg = Raphael(svg3, width, height);
+	    var tr1 = svg.path(" M 60 205 L 130 165 L 140 275 L 60 205").attr({"stroke":"#3981cb","stroke-width":"2","fill":"#3981cb","opacity":0});
+	    var tr2 = svg.path(" M 90 365 L 100 315 L 130 335 L 90 365").attr({"stroke":"#3981cb","stroke-width":"2","fill":"#3981cb","opacity":0});
+	    var tr3 = svg.path(" M 70 475 L 130 555 L 200 440").attr({"stroke":"#3981cb","stroke-width":"2","fill":"#3981cb","opacity":0});
+	    var tr4 = svg.path(" M 40 625 L 60 655 L 70 625").attr({"stroke":"#3981cb","stroke-width":"2","fill":"#3981cb","opacity":0});
+
+	    var animation1 = Raphael.animation({"opacity":1},1000,"<").delay(500);
+	    var animation2 = Raphael.animation({"opacity":0.7},1000,"<").delay(1000);
+	    var animation3 = Raphael.animation({"opacity":1},1000,"<").delay(1500);
+	    var animation4 = Raphael.animation({"opacity":0.8},1000,"<").delay(2000);
+	    tr1.animate(animation1);
+	    tr2.animate(animation2);
+	    tr3.animate(animation3);
+	    tr4.animate(animation4);
+	    paperSet.push(svg);
 	}
 
 
 	var clean = function(){
-	    d3.selectAll("svg").remove();
+	    for(var i in paperSet){
+	        paperSet[i].remove();
+	    }
 	}
+	var paintMap = function(final){
+	    var r1 = Raphael("techs", 600, 400);
+	    var r = Raphael("holder", 600, 600),
+	        R = 250, param = {stroke: "red", "stroke-width": 30};
+
+
+
+	    var tech1 = [{
+	        name:"Es5 / Jquery",
+	        num:80
+	    },{
+	        name:"Css3 / LESS",
+	        num:70
+	    },{
+	        name:"Html5 / Jade",
+	        num:60
+	    },{
+	        name:"Bootstrap",
+	        num:60
+	    },{
+	        name:"Coffeescript / ES6",
+	        num:40
+	    }];
+
+	    var tech2 = [{
+	        name:"Backbone",
+	        num:70
+	    },{
+	        name:"Angular",
+	        num:65
+	    },{
+	        name:"NodeJs / Express",
+	        num:40
+	    },{
+	        name:"Jekyll",
+	        num:38
+	    },{
+	        name:"RaphaelJs",
+	        num:55
+	    }
+	    ];
+
+	    var tech3 = [{
+	        name:"Gulp / Bower",
+	        num:65
+	    },{
+	        name:"WebPack",
+	        num:40
+	    },{
+	        name:"Mock / underscore",
+	        num:60
+	    },{
+	        name:"RequireJs",
+	        num:30
+	    },{
+	        name:"MarkDown",
+	        num:70
+	    }];
+
+	    var tech4 = [{
+	        name:"Webstorm",
+	        num:65
+	    },{
+	        name:"sublime",
+	        num:30
+	    },{
+	        name:"SmartGit",
+	        num:45
+	    },{
+	        name:"chrome",
+	        num:60
+	    },{
+	        name:"Github",
+	        num:69
+	    }];
+
+	    r.customAttributes.arc = function (value, total, R,center,color) {
+	        var alpha = 360 / total * value,
+	            a = (90 - alpha) * Math.PI / 180,
+	            x = 300 + R * Math.cos(a),
+	            y = 300 - R * Math.sin(a),
+	            path;
+	        if (total == value) {
+	            path = [["M", 300, 300 - R], ["A", R, R, 0, 1, 1, 299.99, 300 - R]];
+	        } else {
+	            path = [["M", 300, 300 - R], ["A", R, R, 0, +(alpha>180), 1, x, y]];
+	        }
+	        if (center == true){
+	            return {path: path, stroke: "hsb(0.58,0.72,0.48)", fill: "hsb(0.58,0.72,0.48)"};
+	        }
+	        return {path: path, stroke: color};
+	    };
+
+
+
+	    var eventTrigger = function(obj,view,datas,color){
+	        cleanAnmit();
+	        obj.animate({"stroke-width":"80","opacity":"0.75"},500,"elastic");
+	         drawTechs(view,datas,color);
+	    }
+
+	    var sec1 = r.path().attr(param).attr({arc: [30, 100, R-105,false,"hsb(0.21,1,0.81)"]}).click(function(){
+	        eventTrigger(this,r1,tech4,"#97d000");
+	        centerTxt.attr({"text":"Tools \n 30%"});
+	    });
+	    var sec2 = r.path().attr(param).attr({arc: [45, 100, R-70,false,"hsb(0.82,0.28,1)"]}).click(function(){
+	        eventTrigger(this,r1,tech3,"#fab5ff");
+	        centerTxt.attr({"text":"Common \n 45%"});
+	    });
+	    var sec3 = r.path().attr(param).attr({arc: [65, 100, R-35,false,"hsb(0.63,0.65,0.84)"]}).click(function(){
+	        eventTrigger(this,r1,tech2,"#4866d9");
+	        centerTxt.attr({"text":"Advanced \n 65%"});
+	    });
+	    var sec4 = r.path().attr(param).attr({arc: [85, 100, R,false,"hsb(0.54,0.86,0.8)"]}).click(function(){
+	        eventTrigger(this,r1,tech1,"#4fa4d9");
+	        centerTxt.attr({"text":"FE-Basic \n 85%"});
+	    });
+	    var sec5 = r.path().attr(param).attr({arc: [100, 100, R-140,true]}).click(function(){
+	        cleanAnmit();
+	        r1.clear();
+	        centerTxt.attr({"text":"Skills"});
+	    });
+	    var centerTxt = r.text("300","300","Skills").attr({"font-size":"40px","fill":"white","stroke":"white" ,"text-anchor":"center"});
+
+	    centerTxt.click(function(){
+	        centerTxt.attr({"text":"Skills"});
+	        cleanAnmit();
+	        r1.clear();
+	    });
+
+
+	        $(".btn").eq(3).unbind().on("click",function(){
+	            eventTrigger(sec1,r1,tech4,"#97d000");
+	            centerTxt.attr({"text":"Tools \n 30%"});
+	        });
+	        $(".btn").eq(2).unbind().on("click",function(){
+	            eventTrigger(sec2,r1,tech3,"#fab5ff");
+	            centerTxt.attr({"text":"Common \n 45%"});
+	        });
+	        $(".btn").eq(1).unbind().on("click",function(){
+	            eventTrigger(sec3,r1,tech2,"#4866d9");
+	            centerTxt.attr({"text":"Advanced \n 65%"});
+	        });
+	        $(".btn").eq(0).unbind().on("click",function(){
+	            eventTrigger(sec4,r1,tech1,"#4fa4d9");
+	            centerTxt.attr({"text":"FE-Basic \n 85%"});
+	        });
+
+	    var cleanAnmit = function(){
+	        sec1.animate({"stroke-width":"30","stroke-opacity":"1"},500,"bounce");
+	        sec2.animate({"stroke-width":"30","stroke-opacity":"1"},500,"bounce");
+	        sec3.animate({"stroke-width":"30","stroke-opacity":"1"},500,"bounce");
+	        sec4.animate({"stroke-width":"30","stroke-opacity":"1"},500,"bounce");
+	    };
+	    /*--techs---*/
+
+	    var drawTechs = function(r1,data,color){
+	        r1.clear();
+	        var l = data.length;
+
+	        for(var i in data){
+	            var lastNum = (data[i].num)/100*600+80;
+	            var add = 60+60*i
+	            console.log(lastNum);
+	            r1.path("M80,"+add).attr({"stroke-width":"40","stroke-opacity":"0.8","stroke":color}).animate({"path":"M80,"+add+"L"+lastNum+","+add},Math.random()*2500,"bounce");
+	            r1.text("100",add,data[i].name).attr({"font-size":"25px","fill":"white","stroke":"white" ,"text-anchor":"start"});
+	        }
+	    }
+	    paperSet.push(r);
+	    paperSet.push(r1);
+	    final();
+	}
+
+
 
 	module.exports = {
 	    "paintingline1":paintingline1,
 	    "paintingline2":paintingline2,
 	    "clean":clean,
-	    "paintingTr":paintingTr
+	    "paintingTr":paintingTr,
+	    "paintMap":paintMap
+
 	}
+
 
 
 
