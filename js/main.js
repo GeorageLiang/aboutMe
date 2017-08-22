5523233d186fae658c537ec93348f4c077a0ce36
GeorageLiang <callmelianghaoxu@gmail.com>
2017-08-22 20:15:08 +0800

    update

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ var swiperAnimate = require('../lib/swiper.animate1.0.2.min').swiperAnimate;
 require('../lib/raphael');
 require('../lib/particle');
 var painting  = require('../lib/paint');
+var util = require('../lib/util');
 
 
 
@@ -26,63 +27,68 @@ var painting  = require('../lib/paint');
 //}
 //
 
-$(document).ready(function () {
-    var mySwiper = new Swiper ('.swiper-container', {
-        // 如果需要分页器
-        direction:'vertical',
-        speed:800,
-        onInit: function(swiper){
-            painting.paintingline1("s1");
-            painting.paintingTr("s1");
-            $('.man').addClass('showUpIn');
+// replace www.test.com with your domain
+if (!util.IsPc()) {
+    $(window).load(()=>{
 
-            particlesJS.load('s4', 'assets/particle.json', function() {});
-            particlesJS.load('s5', 'assets/particle.json', function() {});
-            particlesJS.load('s6', 'assets/particle.json', function() {});
+        var mySwiper = new Swiper ('.swiper-container', {
+            // 如果需要分页器
+            direction:'vertical',
+            speed:800,
+            onInit: function(swiper){
+                $('.loading').hide();
+                $('#s1 .lineUp').addClass('lineUp-on');
+                $('#s1 .lineDown').addClass('lineDown-on');
+                particlesJS.load('s4', 'assets/particle.json', function() {});
+                particlesJS.load('s5', 'assets/particle.json', function() {});
+                particlesJS.load('s6', 'assets/particle.json', function() {});
 
-            swiperAnimateCache(swiper); //隐藏动画元素
-            swiperAnimate(swiper); //初始化完成开始动画
-        },
-        onSlideChangeEnd: function(swiper){
-            swiperAnimate(swiper); //每个slide切换结束时也运行当前slide动画
-            switch(mySwiper.realIndex){
-                case 0:
-                    painting.clean();
-                    painting.paintingline1("s1");
-                    painting.paintingTr("s1");
-                    $('.man').addClass('showUpIn');
-                    break;
-                case 1:
-                    $('.man').removeClass('showUpIn');
-                    painting.clean();
-                    painting.paintingline2("s2");
-                    break;
-                case 2:
-                    painting.clean();
-
-                    break;
-                case 3:
-                    painting.clean();
-                    painting.paintMap(function(){
-                    });
-                    break;
-                case 4:
-                    painting.clean();
-                    break;
-                case 5:
-                    painting.clean();
-                    break;
-            }
-        },
+                swiperAnimateCache(swiper); //隐藏动画元素
+                swiperAnimate(swiper); //初始化完成开始动画
+            },
+            onSlideChangeEnd: function(swiper){
+                swiperAnimate(swiper); //每个slide切换结束时也运行当前slide动画
+                switch(mySwiper.realIndex){
+                    case 0:
+                        painting.clean();
+                        $('#s1 .lineUp').addClass('lineUp-on');
+                        $('#s1 .lineDown').addClass('lineDown-on');
+                        $('#s2 .lineUp').removeClass('lineUp-on');
+                        $('#s2 .lineDown').removeClass('lineDown-on');
+                        $('.man').addClass('showUpIn');
+                        break;
+                    case 1:
+                        $('.man').removeClass('showUpIn');
+                        $('#s1 .lineUp').removeClass('lineUp-on');
+                        $('#s1 .lineDown').removeClass('lineDown-on');
+                        $('#s2 .lineUp').addClass('lineUp-on');
+                        $('#s2 .lineDown').addClass('lineDown-on');
+                        painting.clean();
+                        break;
+                    case 2:
+                        painting.clean();
+                        $('#s2 .lineUp').removeClass('lineUp-on');
+                        $('#s2 .lineDown').removeClass('lineDown-on');
+                        break;
+                    case 3:
+                        painting.clean();
+                        painting.paintMap(function(){
+                        });
+                        break;
+                    case 4:
+                        painting.clean();
+                        break;
+                    case 5:
+                        painting.clean();
+                        break;
+                }
+            },
+        });
     });
 
-
-})
-$(window).load(()=>{
-
-});
-
-
+} else {
+    location.href = 'http://me.geoliang.top/web/';
+}
 
 //
 //$('.add').on('click',() =>{
